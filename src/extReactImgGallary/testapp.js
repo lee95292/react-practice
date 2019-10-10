6d97ede8e9b664f6b0dafa259f0e4583dac85c96
lee95292 <lee95292@naver.com>
2019-10-10 18:10:09 +0900

    [Add] install image-slide

---
+++ src/extReactImgGallary/testapp.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ImageGallery from 'react-image-gallery';
+import '../style/appstyle.css';
+
+const images = [
+    {
+        original:'/img/reactlogo.png',
+        sizes:'100%'
+    },
+    {
+        original:'/img/reactlogo.png',
+        sizes:'100%'
+    }
+];
+
+class MyGallery extends React.Component {
+    constructor(){
+        super();
+        this.state={
+            showThumbnails:false,
+            showFullscreenButton:true,
+            showPlayButton:false,
+            showNav:true
+        }
+        
+    }
+    render() {
+      return (
+        <section id="app">
+            <ImageGallery 
+                items={images}
+                showFullscreenButton={this.state.showFullscreenButton} 
+                showThumbnails={this.state.showThumbnails} 
+                showPlayButton={this.state.showPlayButton} 
+                showNav={this.state.showNav}/>
+        </section>
+
+      );
+    }
+  }
+  
+  export default MyGallery;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import LoginControl from './7userView/loginControll';
 import ActionLink from './6eventHandling/eventApp'
 import Toggle from './6eventHandling/toggle'
-
+import MyGallery from './extReactImgGallary/testapp.js';
 /*
     6. Event handling App
 */
@@ -13,6 +13,8 @@ class ReturnALL extends React.Component{
     render(){
         return(
             <div>
+                <p>My Gallery</p>
+                <MyGallery />
                 <p>Login Controll App</p>
                 <LoginControl />
                 <hr/>
