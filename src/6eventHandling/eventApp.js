f3b7ef47be2c5fd6e59cd19f6d79eb428f32a262
lee95292 <lee95292@naver.com>
2020-03-18 14:11:08 +0900

    testing..

--- src/6eventHandling/eventApp.js
+++ src/6eventHandling/eventApp.js
@@ -1,15 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-function ActionLink(props){
-    function handleClick(e){
-        e.preventDefault();
-        console.log('The Link was clicked');
-    }
-    return (
-        <a href="#" onClick={handleClick}>click link</a>
-    );
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+
+function ActionLink(props) {
+  function handleClick(e) {
+    e.preventDefault();
+    console.log("The Link was clicked");
+
+    axios
+      .get(
+        "http://newsapi.org/v2/everything?q=bitcoin&from=2020-02-18&sortBy=publishedAt&apiKey=99ef42fc4cf0491c928597b08fd47354"
+      )
+      .then(res => {
+        console.log(res);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+  return (
+    <a href="#" onClick={handleClick}>
+      click link
+    </a>
+  );
 }
 
-ReactDOM.render(<ActionLink />,document.getElementById('root'));
-    
+ReactDOM.render(<ActionLink />, document.getElementById("root"));
+
 export default ActionLink;
